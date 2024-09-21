d89b0d9eb1daef1d4b0f233c60cf141f67ba6fa4
mdnurui579 <mdnuruli579@gmail.com>
2024-09-21 09:32:07 +0530

    project created

---
+++ src/magicalArena/Main.java
@@ -0,0 +1,10 @@
+package magicalArena;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/magicalArena/Player.java
@@ -0,0 +1,33 @@
+package magicalArena;
+
+public class Player {
+	private int health;
+    private int strength;
+    private int attack;
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+    public int getHealth() {
+        return health;
+    }
+
+    public void reduceHealth(int damage) {
+        this.health -= damage;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+    
+
+}
